bde8f86aeb872e5d2e664eff2b8ec5d036fc6dae
LirenXH <1577973328@qq.com>
2022-03-04 19:16:54 -0800

    chenggong

--- doubleswitch/doubleswitch.cpp
+++ doubleswitch/doubleswitch.cpp
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-
-while (???) {
-  int a = rand() & 1;
-  int b = rand() & 1;
-  top->a = a;
-  top->b = b;
-  top->eval();
-  printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-  assert(top->f == a ^ b);
+#include "Vtop.h" 
+Vtop *top;
+int main(int argc, char** argv) {
+    Verilated::commandArgs(argc, argv);   // Remember args
+    top = new Vtop;             // Create model
+        while (true) {
+          int a = rand() & 1;
+          int b = rand() & 1;
+          top->a = a;
+          top->b = b; 
+          top->eval();
+          printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+          assert(top->f == a ^ b);
+        }
+        top->final();
+        delete top; 
 }
