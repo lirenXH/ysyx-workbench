f2023b348619984ca610e4f3c38712275d711fb4
LirenXH <1577973328@qq.com>
2022-03-07 19:36:19 -0800

    NJU-ProjectN/am-kernels ics2021 initialized without tracing
    NJU-ProjectN/am-kernels adc316af6e482e6444a9bd68bafc3a57e2cafdbc cpu-tests,Makefile: support target `gdb`

--- doubleswitch/doubleswitch.cpp
+++ doubleswitch/doubleswitch.cpp
@@ -1,20 +1,32 @@
+#include "verilated_vcd_c.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 #include "Vtop.h" 
-Vtop *top;
+#include "verilated.h"
 int main(int argc, char** argv) {
-    Verilated::commandArgs(argc, argv);   // Remember args
-    top = new Vtop;             // Create model
+    VerilatedContext* contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vtop* top = new Vtop{contextp};
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->trace(tfp,99);
+    tfp->open("obj_dir/sim1.vcd");
         while (true) {
+	  contextp->timeInc(1);
           int a = rand() & 1;
           int b = rand() & 1;
           top->a = a;
           top->b = b; 
           top->eval();
+	  tfp->dump(contextp->time());
           printf("a = %d, b = %d, f = %d\n", a, b, top->f);
           assert(top->f == a ^ b);
         }
         top->final();
+	tfp->close();
         delete top; 
+	delete contextp;
+	delete tfp;
+	return 0;
 }
