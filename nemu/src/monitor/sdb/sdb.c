94a6b9ae4ac1aeacfc4c509056e261e52ded6fb1
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-10 22:19:58 -0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:19:58 up  4:21,  1 user,  load average: 0.50, 0.13, 0.04

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -53,6 +53,7 @@ static int cmd_si(char *args) {
 static int cmd_info(char *args) {
   char *str = strtok(args," ");
   if(strcmp(str, "r") != 0){
+    printf("he");
     isa_reg_display();
     return 0;
   }
