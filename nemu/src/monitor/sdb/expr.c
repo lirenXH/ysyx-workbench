fb936ae4791151e8b7f9a45eecbe57614f44993c
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-26 00:50:56 -0700

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:50:56 up 52 min,  1 user,  load average: 0.09, 0.07, 0.05

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -100,7 +100,7 @@ static bool make_token(char *e) {
           case 41 : tokens->type = 41;
           case 47 : tokens->type = 47;
           case 48 : tokens->type = 47;
-          case 49 : printf("!1!");
+          case 49 : printf("!1!,%d",tokens->type);
           case 50 : printf("!2!");
           default: TODO();
         }
