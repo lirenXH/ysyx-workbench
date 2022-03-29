8f373622c22c92b67e9551d667f2c0ba669d1924
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-29 04:57:19 -0700

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:57:19 up  1:39,  1 user,  load average: 0.02, 0.01, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -74,7 +74,7 @@ static int nr_token __attribute__((used))  = 0;
 
 int valve(int a){
 	int aa;
-	sscanf(tokens[a-1].str, "%d", &aa);
+	sscanf(tokens[a-2].str, "%d", &aa);
 	printf("aa = %d,nr_token = %d\n",aa,a);
 	return 0;
 }
