76c7feb6506598af33a29716ac6291b71bdf06af
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-31 05:10:27 -0700

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:10:27 up 41 min,  1 user,  load average: 0.07, 0.11, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -114,6 +114,7 @@ int check_parentheses(int p,int q){
     /* The expression is surrounded by a matched pair of parentheses.
      * If that is the case, just throw away the parentheses.
      */
+     printf("1111\n");
     return eval(p + 1, q - 1);
   }
   else {
