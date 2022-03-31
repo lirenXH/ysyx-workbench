b06449cd426b6b4c8de3c9c1f9ace355112c0d3d
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-31 04:58:01 -0700

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:58:01 up 29 min,  1 user,  load average: 0.00, 0.01, 0.06

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -120,14 +120,14 @@ int check_parentheses(int p,int q){
     for(i=0;i<=nr_token;i++){
       if(strcmp("*",tokens[i].str)||strcmp("+",tokens[i].str)||strcmp("-",tokens[i].str)||strcmp("/",tokens[i].str)){
         for (j = 0; j <= i; j++){
-          if(strcmp("(",tokens[j].str))   //为(
+          if(!strcmp("(",tokens[j].str))   //为(
             flag1 = 1;
         }
         for (j = i+1; j <= nr_token; j++){    //bug ()+()!!!!!!!!
-          if(strcmp(")",tokens[j].str))   //为)
+          if(!strcmp(")",tokens[j].str))   //为)
             flag2 = 1;
         }
-        if((flag1&&flag2)){     //已经筛选（） 还差检查优先级
+        if(!(flag1&&flag2)){     //已经筛选（） 还差检查优先级
           if(strcmp("+",tokens[i].str)||strcmp("-",tokens[i].str)){
             op = i;
             printf("在%d处找到主运算符+ -",i);
