bad80ee18941702d734f3421a5a27181d3107ae8
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 00:32:06 -0700

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:32:06 up  1:02,  1 user,  load average: 0.03, 0.07, 0.04

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -145,7 +145,7 @@ int check_parentheses(int p,int q){
     val2 = eval(op + 1, q);
     if(val1==0||val2==0){
     	printf("出现除0 error\n");
-    	return -1;
+    	assert(0);
     	}
 		op1 = tokens[op].type;
     switch (op1) {
