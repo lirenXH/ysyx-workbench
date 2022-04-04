926956f1695a0a3a1d5480a589741e344a5a8e23
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 19:25:47 -0700

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:25:47 up  1:43,  1 user,  load average: 0.16, 0.08, 0.03

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -158,8 +158,10 @@ int count_r=0;
         				printf("faxian +-!!\n");
         				for(x=i;x<=k;x++){
         					printf("tokens[x]=%d\n",tokens[x].type);
-        					if(tokens[x].type==40)
+        					if(tokens[x].type==40){
+        						printf("l++");
         						count_l++;
+        						}
         					if(tokens[x].type==41)
         						count_l--;
         				}
