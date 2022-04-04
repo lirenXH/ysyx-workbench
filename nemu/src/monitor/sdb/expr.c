1a7542ab1823919a32002878b0125f2ab4c461ec
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 18:38:37 -0700

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:38:37 up 55 min,  1 user,  load average: 0.04, 0.09, 0.04

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -176,10 +176,12 @@ int count_r=0;
 						      break;
         				}
         			}
-        			op = i;
-		          printf("在%d处找到主运算符2* /%d\n",i,tokens[i].type);
-		          //printf("op:%d   q:%d\n",op,q);
-		          break;
+        			else{
+		      			op = i;
+				        printf("在%d处找到主运算符2* /%d\n",i,tokens[i].type);
+				        //printf("op:%d   q:%d\n",op,q);
+				        break;
+		          }
         		}
           }
           else if(tokens[i].type==43||tokens[i].type==45){
