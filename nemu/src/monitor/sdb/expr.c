7dbc46725e69f0e76964c5defc8a011dcb3ba9d8
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-06 06:19:32 -0700

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:19:32 up 5 min,  1 user,  load average: 0.09, 0.39, 0.24

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -1,15 +1,17 @@
 #include <isa.h>
-
+#include <string.h>
 /* We use the POSIX regex functions to process regular expressions.
  * Type 'man regex' for more information about POSIX regex functions.
  */
 #include <regex.h>
 
-enum {                                             // All expres type 
-  TK_NOTYPE = 256, TK_EQ,NUMBER,// '+','-','*','/'
+enum {
+  TK_NOTYPE = 256, TK_EQ,NUM = 1
+
   /* TODO: Add more token types */
 
 };
+
 
 static struct rule {
   const char *regex;
@@ -20,21 +22,20 @@ static struct rule {
    * Pay attention to the precedence level of different rules.
    */
 
-  {" +", TK_NOTYPE},    // spaces: will discard
-  {"\\+", '+'},         // plus
-  {"\\-", '-'},
-  {"\\*", '*'},
-  {"\\/", '/'},
-  {"==", TK_EQ},        // equal
-  {"\\(", '('},
-  {"\\)", ')'},
-  {"\\b[0-9]+\\b", NUMBER}
-
+  {" +" , TK_NOTYPE},    // spaces
+  {"\\+", '+'},         // plus 43
+  {"==" , TK_EQ},        // equal
+  {"-"  , '-'},		//reduce 45
+  {"\\(", '('},		//( 40
+  {"\\)", ')'},		//) 41
+  {"\\*" , '*'},   // * 42
+  {"/" , '/'},   /// 47
+  {"[0-9]+" , NUM}, //NUM
 };
 
 #define NR_REGEX ARRLEN(rules)
 
-static regex_t re[NR_REGEX] = {};                                               // All rules index.
+static regex_t re[NR_REGEX] = {};
 
 /* Rules are used for many times.
  * Therefore we compile them only once before any usage.
@@ -45,8 +46,8 @@ void init_regex() {
   int ret;
 
   for (i = 0; i < NR_REGEX; i ++) {
-    ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);                       // token->pattern.
-    if (ret != 0) {                                                            // error
+    ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
+    if (ret != 0) {
       regerror(ret, &re[i], error_msg, 128);
       panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
     }
@@ -58,21 +59,140 @@ typedef struct token {
   char str[32];
 } Token;
 
-
 static Token tokens[32] __attribute__((used)) = {};
-static int nr_token __attribute__((used))  = 0;                     // token index
+static int nr_token __attribute__((used))  = 0;
+
+//----------------------------------------------------------------------------
+
+int check_parentheses(int p,int q){
+int count_l=0;// qingling 00!!!!
+int count_r=0;
+  if(tokens[p].type==40&&tokens[q].type==41){
+  	for(int ii=p+1;ii<q;ii++){				//判断左右两侧括号是否匹配
+  		if(tokens[ii].type==40)
+  			count_l++;
+  		if(tokens[ii].type==41)
+  			count_r++;
+  	}
+  	if(count_l==count_r){
+  		//printf("YES ()\n");
+  		count_l=0;
+  		count_r=0;
+    	return 1;
+    	}
+    else{
+			//printf("NO1 () count_l:%d  count_r:%d\n",count_l,count_r);
+			count_l=0;
+			count_r=0;
+		  return 0;
+  	}
+   }
+  else{
+  	//printf("NO2 ()\n");
+    return 0;
+  }
+}
+
+int find_op(int p, int q){
+ 	int i;
+ 	int op_place = 0;
+ 	int youxian = 0;
+ 	int zuiyou = 0;
+ 	int count_l=0;
+ 	for(i=p;i<=q;i++){
+			if(tokens[i].type==40){
+  			count_l++;i++;
+  			while(1){
+  				if(tokens[i].type==40)
+  					count_l++;
+  				else if(tokens[i].type==41)
+  					count_l--;
+  				if(count_l==0)
+  					break;
+  				i++;	
+  			}
+  		}
+  		else if(tokens[i].type==43||tokens[i].type==45||tokens[i].type==42||tokens[i].type==47){
+				if(tokens[i].type==43||tokens[i].type==45)
+					youxian = 2;
+				else
+					youxian = 1;
+				if(youxian >= zuiyou){
+					zuiyou = youxian;
+					op_place = i;
+				}
+  		}
+ 	}
+ 	return op_place;
+}
+
+
+
+
+ int eval(int p, int q) {
+  int op = 0;
+  int val1,val2;
+  int aa,op1;
+  //printf("p=%d,q=%d\n",p,q);
+  if (p > q) {
+    /* Bad expression */
+    printf("p > q! error~\n");
+    assert(0);
+  }
+  else if (p == q) {
+    /* Single token.
+     * For now this token should be a number.
+     * Return the value of the number.
+     */
+	  sscanf(tokens[p].str, "%d", &aa);
+	  printf("NUM : %d\n",aa);
+    return aa;
+  }
+  else if (check_parentheses(p, q) == 1) {
+    /* The expression is surrounded by a matched pair of parentheses.
+     * If that is the case, just throw away the parentheses.
+     */
+    printf("去括号\n");
+    return eval(p + 1, q - 1);
+  }
+  else {
+    
+  	op = find_op(p,q);
+    val1 = eval(p, op - 1);
+    val2 = eval(op + 1, q);
+    printf("val1= %d val2= %d\n",val1,val2);
+    if(val1==0||val2==0){
+    	printf("/0    error~\n");
+    	assert(0);   //暂定
+    	}
+		op1 = tokens[op].type;
+    switch (op1) {
+      case '+': return val1 + val2;
+      case '-': return val1 - val2;/* ... */
+      case '*': return val1 * val2;/* ... */
+      case '/': return val1 / val2;/* ... */
+      default: assert(0);
+    }
+  }
+}
+
+
+//----------------------------------------------------------------------------
 
 static bool make_token(char *e) {
   int position = 0;
   int i;
+  int result;
+  //int j;
   regmatch_t pmatch;
 
   nr_token = 0;
 
   while (e[position] != '\0') {
+    /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {             //The length of the array is 1
-        char *substr_start = e + position;                                     
+      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+        char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
@@ -86,166 +206,50 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
-          
-          case TK_NOTYPE: { printf("TK_NOTYPE is ok!\n");  break;     }  
-          case TK_EQ:  {    printf("TK_EQ is ok!\n");   strncpy(tokens[nr_token].str, substr_start,substr_len); tokens[nr_token].type=rules[i].token_type; nr_token=nr_token+1; break;}
-          case NUMBER: {    printf("NUMBER is ok!\n");  strncpy(tokens[nr_token].str, substr_start,substr_len); tokens[nr_token].type=rules[i].token_type; nr_token=nr_token+1; break;}
-          case '+':    {    printf("+ is ok!\n");       strncpy(tokens[nr_token].str, substr_start,substr_len); tokens[nr_token].type=rules[i].token_type; nr_token=nr_token+1; break;}
-          case '-':    {    printf("- is ok!\n");       strncpy(tokens[nr_token].str, substr_start,substr_len); tokens[nr_token].type=rules[i].token_type; nr_token=nr_token+1; break;}
-          case '*':    {    printf("* is ok!\n");       strncpy(tokens[nr_token].str, substr_start,substr_len); tokens[nr_token].type=rules[i].token_type; nr_token=nr_token+1; break;}
-          case '/':    {    printf("/ is ok!\n");       strncpy(tokens[nr_token].str, substr_start,substr_len); tokens[nr_token].type=rules[i].token_type; nr_token=nr_token+1; break;}
-          case '(':    {    printf("( is ok!\n");       strncpy(tokens[nr_token].str, substr_start,substr_len); tokens[nr_token].type=rules[i].token_type; nr_token=nr_token+1; break;}
-          case ')':    {    printf(") is ok!\n");       strncpy(tokens[nr_token].str, substr_start,substr_len); tokens[nr_token].type=rules[i].token_type; nr_token=nr_token+1; break;}
-          default: printf("Error!\n"); break;
-
+        	case 256 : break;
+          case 43 : {tokens[nr_token].type = 43;nr_token++;break;};
+          case 42 : {tokens[nr_token].type = 42;nr_token++;break;};
+          case 45 : {tokens[nr_token].type = 45;nr_token++;break;};
+          case 40 : {tokens[nr_token].type = 40;nr_token++;break;};
+          case 41 : {tokens[nr_token].type = 41;nr_token++;break;};
+          case 47 : {tokens[nr_token].type = 47;nr_token++;break;};
+          case NUM  : {tokens[nr_token].type = 1 ;strncpy(tokens[nr_token].str,substr_start,substr_len);nr_token++;break;};
+          default: return 0;
         }
-        
+
         break;
       }
-
+    }
+		
+		//for(j=0;j<nr_token;j++)
+							//printf("type:%d		str:%s\n",tokens[j].type,tokens[j].str);
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
     }
   }
-
-}
-
-
-/*
-      printf("--------------------------------------------Tokens array------------------------------------------\n");
-      for(int i =0; i<32;i++){
-            printf("The %s tokens is %d type!\n",tokens[i].str,tokens[i].type);
-      }  
-*/
+  result = eval(0,nr_token-1);
+  	printf("结果为:%d\n",result);
+  for(int qing=0;qing<32;qing++){					//每次运算后清空结构体 防止溢出
+  	tokens[qing].type = 0;
+  	for(int qing2=0;qing2<32;qing2++)
+  		tokens[qing].str[qing2] = 0;
+  }
   return true;
 }
 
-//---------------
-//----------------------------------------------------------------------------
-
-
-//----------------
-word_t eval(int p, int q);
 
 word_t expr(char *e, bool *success) {
   if (!make_token(e)) {
-    *success = false;
+    //*success = false;       除0是否跳出 
     return 0;
   }
-  int out;//nr_token;
-  printf("nr_token is %d!\n",nr_token);
-//  out= eval(0,nr_token-1);
-//  printf("nr_token = %d\n",nr_token);
-  printf(" %s = %d\n",e,out);
-  return 0;
-}
 
-
-bool check_parentheses(int p, int q){
-  /*To check parentheres*/
-  printf("-------------check parentheses-------------\n");
-  int left=0;
-  if(tokens[p].type!='(' || tokens[q].type!=')'){
-    printf("At least two parentheses\n");
-    return false;
-  }else{
-    for(int i=p;i<=q;i++){
-      if(tokens[i].type=='(') left++;
-      else if(tokens[i].type==')') left--;
-
-      if(left==0 && i<q) {
-        printf("This is a state that is ( + )*( - )\n");
-        return false;
-      }
-    }
-  }
-  if(left!=0) {
-    printf("Too more paretheses\n");
-    return false;
-  }
-  return true;
-}
-
-word_t pir(int type){
-  int i=0;
-  if(type=='+' || type=='-') i=2;
-  else if(type=='*' || type=='/' ) i=1;
-
-  return i;
-}
-
-word_t find_op(int p, int q){
-  printf("-------------------Find main op-------------------------\n");
-  int op_index=0; // save main op
-  int left=0;
-  int pr=0;
-  if(p>q) assert(0);
-  for(int i=p;i<=q;i++){
-    if(tokens[i].type=='('){
-      left++;  i++;                                       //detect next i;
-      while(1){
-        if(tokens[i].type=='(') {left++;}
-        else if(tokens[i].type==')') {left--;}         
-        if(left==0) break;
-        i++;
-      }                                                   // here is tropuble!
-    } else if(tokens[i].type==NUMBER) {printf("This is a number!\n");continue;}
-    else if(tokens[i].type=='+' || tokens[i].type=='-' ||tokens[i].type=='*' ||tokens[i].type=='/'){
-        printf("This is a op!\n");
-        if(pir(tokens[i].type)>=pr){
-
-          pr=pir(tokens[i].type);
-          op_index=i;
-          printf("pr is %d,op_index is %d\n",pr,op_index);
-        }
-    
-    }
-  }
-
-  printf("The main op is %s and position is %d\n",tokens[op_index].str,op_index);            
-  return op_index;
-}
-
-word_t eval(int p, int q){
+  /* TODO: Insert codes to evaluate the expression. */
   
-  int op;
-  char val1,val2;
-  int op_type=' ';
+//----------------------------------------------------------------------------
 
-  if (p > q) {
-  printf("Bad expression \n");
-  assert(0);
-  }
-  else if (p == q) {
+//-----------------------------------------------------------------------------  
 
-    int out=0;
-    sscanf(tokens[p].str, "%d", &out);
-//    printf("val is %d\n",out);
-    return out;
-
-  }
-  else if (check_parentheses(p, q) == true) {  
-    return eval(p + 1, q - 1);
-  }
-  else {
-    printf("-----------Main op and value-----------------------\n");
-    printf(" We should do more things here.\n");
-    
-    op = find_op(p,q)   ;     //the position of 主运算符 in the token expression;
-    op_type = tokens[op].type;
-
-    val1 = eval(p, op - 1); printf("val1 is a single number, and value is = %d\n",val1);
-    val2 = eval(op + 1, q); printf("val2  is a single number, and value is = %d\n",val2);
-
-    switch (op_type) {
-      case '+': return val1 + val2;
-      case '-': return val1 - val2;
-      case '*': return val1 * val2;
-      case '/': return val1 / val2;
-      default: assert(0);
-    }
-//    return 0;
-  }
   return 0;
 }
