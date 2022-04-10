2ed2cac7b41e018d25c3831aa1b618eefa7d70e3
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-10 06:37:38 -0700

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:37:38 up 10:54,  1 user,  load average: 0.46, 0.26, 0.18

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -22,7 +22,7 @@ enum {
 static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
-static word_t immJ(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 20) | BITS(i, 30, 21) | (BITS(i, 20, 20) << 11) | (BITS(i, 19, 12) << 12); }
+static word_t immJ(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 20) | (BITS(i, 30, 21) << 1) | (BITS(i, 20, 20) << 11) | (BITS(i, 19, 12) << 12); }
 
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
@@ -55,7 +55,7 @@ static int decode_exec(Decode *s) {
   
   INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(dest) = src1);
   INSTPAT("??????? ????? ????? ??? ????? 00100 11", addi   , I, R(dest) = src1 + src2);
-  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(dest) = s->pc + (word_t)4,s->dnpc = s->dnpc + src1); printf("src1=%ld\n",src1);
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, R(dest) = s->pc + (word_t)4,s->dnpc = s->dnpc + src1);
   INSTPAT("??????? ????? ????? 010 ????? 11001 11", jalr   , I, word_t t = s->pc + 4;s->dnpc = (src1 + src2) & ~((word_t)1);R(dest) = t);
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
