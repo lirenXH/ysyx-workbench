466bb79ff3089b3bed6e7052ab2d409bfbe260c1
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-24 10:51:53 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:51:53 up 57 min,  1 user,  load average: 1.09, 0.47, 0.40

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -17,12 +17,12 @@ int sprintf(char * out,const char * fmt, ...)
 {
 	va_list ap;
 	char *sval;
-  const char *p;
+    const char *p;
 	int ival;
 	char buf[30];
-  char buff[30];
+    char buff[30];
 	int strcnt = 0;
-  int count=0;
+    int count=0;
 	///
 
 	char str_buf[64];
@@ -40,7 +40,8 @@ int sprintf(char * out,const char * fmt, ...)
                     count++;
 					continue;
 			}
-
+			memset(buff,'\0',sizeof(buff));
+			memset(buf,'\0',sizeof(buf));
 			switch(*++p)
 			{
 			case 'd':
@@ -50,25 +51,28 @@ int sprintf(char * out,const char * fmt, ...)
                     while(ival>=10){
                         buff[len]=(char)('0'+ival%10);
                         ival=ival/10;
-                        //printf("buff=%s\n",buff);
+                        //printf("buff=%s|\n",buff);
                         len++;
                     }
                     buff[len]=(char)('0'+ival%10);
-                    //printf("buff=%s\n",buf);
+                    //printf("buff=%s|\n",buff);
                     int lenn =len;
                     for(int ii = 0; ii <= len ;ii++){
                         buf[ii]=buff[lenn];
                         lenn--;
                     }
+					//printf("buf=%s|\n",buf);
 					for(i = 0; buf[i] != '\0'; i++)
 					{
 							*str_buf_p = buf[i];
 							str_buf_p ++;
 							strcnt ++;
-              count++;
+                            count++;
 					}
-          for(i=0;i<=len;i++)
-            buf[i] = '\0';
+                    memset(buff,'\0',sizeof(buff));
+					memset(buf,'\0',sizeof(buf));
+					//printf("00:buff=%s\n",buff);
+					//printf("00:buf=%s\n",buf);
 					break;
 			case 's':
 					sval = va_arg(ap, char *);
@@ -78,14 +82,14 @@ int sprintf(char * out,const char * fmt, ...)
 							str_buf_p ++;
 							strcnt ++;
 							sval ++;
-              count++;
+                            count++;
 					}
 					break;
 			}
 	}
 	va_end(ap);           
     memcpy(out,str_buf,strcnt); 
-	out[strcnt]=0;          
+	out[strcnt]='\0';          
     return count-1;
 }
 
