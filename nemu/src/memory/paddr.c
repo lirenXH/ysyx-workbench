8f217aa9380f68aa320759d987db7adfd4f6e468
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-26 15:38:10 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:38:10 up  7:54,  1 user,  load average: 0.33, 0.37, 0.30

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -14,13 +14,13 @@ paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
-  printf("rmem:%lx\n",ret);
+  IFDEF(MTRACE, printf("rmem:%lx\n",ret));
   return ret;
 }
 
 static void pmem_write(paddr_t addr, int len, word_t data) {
   host_write(guest_to_host(addr), len, data);
-  printf("wmem:0x%x data:0x%lx\n",addr,data);
+  IFDEF(MTRACE, printf("waddr:0x%x data:0x%lx\n",addr,data));
 }
 
 static void out_of_bound(paddr_t addr) {
