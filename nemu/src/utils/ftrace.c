0868780f7a2874b31c269f9854af544b1838adef
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 14:14:57 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:14:57 up  9:58,  1 user,  load average: 0.52, 0.31, 0.11

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -7,6 +7,7 @@ void init_ftrace(const char *ftrace_file) {
   ftrace_fp = stdout;
   if (ftrace_file != NULL) {
     FILE *fp = fopen(ftrace_file, "r");
+    printf("ftrace file opened!\n");
     Assert(fp, "Can not open elf'%s'", ftrace_file);
     ftrace_fp = fp;
   }
