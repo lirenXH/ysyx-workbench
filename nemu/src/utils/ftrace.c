f6169e25f00e86e911f7526783d05c453ee32b64
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 14:51:48 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:51:48 up 10:35,  1 user,  load average: 0.52, 0.33, 0.27

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -1,14 +1,21 @@
 #include <common.h>
 #include <elf.h>
 extern uint64_t g_nr_guest_inst;
-FILE *ftrace_fp = NULL;
+FILE *fp = NULL;
 
 void init_ftrace(const char *ftrace_file) {
-  ftrace_fp = stdout;
+  fp = stdout;
+  char str[20];
+  int readcount;
   if (ftrace_file != NULL) {
-    FILE *fp = fopen(ftrace_file, "r");
-    printf("ftrace file opened!\n");
+    FILE *fp = fopen(ftrace_file, "rb");
+    memset(str,0,sizeof(str));
+    readcount=fread(str,1,5,fp);
+    if(str[0]!=0x7f|| str[1]!='E'|| str[2]!='L'|| str[3]!='F')
+        printf(" is not an ELF file\n");
+    else
+        printf("readcount = %d\n",readcount);
     Assert(fp, "Can not open elf'%s'", ftrace_file);
-    ftrace_fp = fp;
+    fp = fp;
   }
 }
