8c10c7dadf9772e9d3aa8c151cff8fbf3cc05c05
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 15:43:11 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:43:11 up 11:26,  1 user,  load average: 0.84, 0.46, 0.38

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -4,6 +4,7 @@ extern uint64_t g_nr_guest_inst;
 FILE *ftrace_fp = NULL;
 
 void init_ftrace(const char *ftrace_file) {
+  printf("file :%s\n",ftrace_file);
   ftrace_fp = stdout;
   int frc;
   char str[20];
@@ -15,15 +16,6 @@ void init_ftrace(const char *ftrace_file) {
     frc=fread(str,1,5,fp);
     printf("frc=%d,str=%s\n",frc,str);
     while(fgets(text,1024,fp)!=NULL){//逐行读取fp1所指向文件中的内容到text中
-        puts(text);//输出到屏幕
-    }
-    while(fgets(text,1024,fp)!=NULL){//逐行读取fp1所指向文件中的内容到text中
-        puts(text);//输出到屏幕
-    }
-    while(fgets(text,1024,fp)!=NULL){//逐行读取fp1所指向文件中的内容到text中
-        puts(text);//输出到屏幕
-    }
-        while(fgets(text,1024,fp)!=NULL){//逐行读取fp1所指向文件中的内容到text中
         puts(text);//输出到屏幕
     }
     Assert(fp, "Can not open elf'%s'", ftrace_file);
