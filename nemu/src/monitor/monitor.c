4fd18892a346ac143c50e0b057e841931f15f56b
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 11:25:52 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:25:52 up  7:09,  1 user,  load average: 1.07, 0.82, 0.56

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -70,7 +70,7 @@ static int parse_args(int argc, char *argv[]) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
-      case 'f': printf("F\n");ftrace_file = optarg; break;
+      case 'f': ftrace_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
       case 1: img_file = optarg; return optind - 1;
       default:
@@ -99,7 +99,7 @@ void init_monitor(int argc, char *argv[]) {
   /* Open the log file. */
   init_log(log_file);
 
-  /* Open the log file. */
+  /* Open the ftrace file. */
   init_ftrace(ftrace_file);
 
   /* Initialize memory. */
