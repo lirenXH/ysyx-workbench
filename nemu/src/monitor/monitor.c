ad5761e2b0a76cdacb9c6d3ad68e706041e0b240
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 10:50:45 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:50:45 up  6:34,  1 user,  load average: 0.54, 0.40, 0.34

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -79,7 +79,7 @@ static int parse_args(int argc, char *argv[]) {
         printf("\t-l,--log=FILE           output log to FILE\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
-        printf("\t-f,--ftrace=FILE        read ftrace wiht elf FILE\n");
+        printf("\t-f,--ftrace=FILE        read the ftrace elf FILE\n");
         printf("\n");
         exit(0);
     }
