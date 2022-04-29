846d9b775bc8c21f3d64a464b398117d861bc26d
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-29 16:04:25 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:04:25 up 11:47,  1 user,  load average: 0.88, 0.40, 0.37

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -2,7 +2,8 @@
 #include <elf.h>
 extern uint64_t g_nr_guest_inst;
 FILE *ftrace_fp = NULL;
-
+char*fname;
+int OStype;
 void init_ftrace(const char *ftrace_file) {
   printf("file :%s\n",ftrace_file);
   ftrace_fp = stdout;
@@ -13,24 +14,15 @@ void init_ftrace(const char *ftrace_file) {
     FILE *fp = fopen(ftrace_file, "rb");
     memset(str,0,sizeof(str));
     memset(text,0,sizeof(text));
-    frc=fread(str,1,5,fp);
-    printf("frc=%d,str=%s\n",frc,str);
-    frc=fread(text,1,1024,fp);
-    printf("frc=%d,str=%s\n",frc,text);
-    frc=fread(text,1,1024,fp);
-    printf("frc=%d,str=%s\n",frc,text);
-    frc=fread(text,1,1024,fp);
-    printf("frc=%d,str=%s\n",frc,text);
-    frc=fread(text,1,1024,fp);
-    printf("frc=%d,str=%s\n",frc,text);
-    frc=fread(text,1,1024,fp);
-    printf("frc=%d,str=%s\n",frc,text);
-    frc=fread(text,1,1024,fp);
-    printf("frc=%d,str=%s\n",frc,text);
-    frc=fread(text,1,1024,fp);
-    printf("frc=%d,str=%s\n",frc,text);
-    frc=fread(text,1,1024,fp);
-    printf("frc=%d,str=%s\n",frc,text);
+    fname = &str[0];
+    frc=fread(str, 1, 5, fp);
+    if(str[0] != 0x7f || str[1] != 'E' || str[2] != 'L' || str[3] != 'F') {
+    printf("%s is not an ELF file%d\n", fname,frc);
+    exit(1);
+    }
+    OStype = str[4] == 1 ? 32 : 64; //判断elf⽂件为32位还是64位
+    printf("魔数检验通过，该⽂件为%d位ELF⽂件\n", OStype);
+
     Assert(fp, "Can not open elf'%s'", ftrace_file);
     ftrace_fp = fp;
   }
