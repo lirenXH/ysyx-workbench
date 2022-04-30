00a1c8661616ffb15bb78c21fcecce253a3d29b6
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-30 15:31:11 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:31:11 up 20:55,  1 user,  load average: 0.57, 0.54, 0.54

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -152,8 +152,9 @@ void ftrace_main(word_t ftpc,uint8_t inst,word_t fdnpc){
     //printf("catch jal/jalr dnpc=%ld\n",fdnpc);
     for(int i=0;i<func[1].ffnum;i++){
       if(fdnpc==func[i].value){
+        printf("%#08lx:",ftpc);
         printf("%ld:",ftpc);
-        for(int j=0;j<space_len;j++)
+        for(int j=0;j<=space_len;j++)
           printf("  ");
         space_len=space_len+2;
         printf("call [%s@%ld]\n",func[i].name,func[i].value);
