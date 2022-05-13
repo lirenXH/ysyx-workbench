7931a0627b277368ad95d1c90ac16c2d38b515a2
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-13 22:00:29 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:00:29 up  1:53,  1 user,  load average: 1.30, 0.58, 0.43

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -10,9 +10,7 @@ struct diff_context_t {
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
-    for (size_t i = 0; i < n; i++) {
-      paddr_write(addr+i, n ,*((uint8_t*)buf+i));
-    }
+      paddr_write(addr, n ,*(uint8_t*)buf);
   } 
   else{
     assert(0);
