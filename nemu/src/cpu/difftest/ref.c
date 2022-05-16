cd229881e48ae96d29862cdebe4d0b4a7b07a175
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-16 14:50:04 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:50:04 up  2:32,  1 user,  load average: 0.29, 0.22, 0.14

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -3,47 +3,49 @@
 #include <difftest-def.h>
 #include <memory/paddr.h>
 
-struct diff_context_t {
-  uint64_t gpr[32];
-  uint64_t pc;
-};
+//struct diff_context_t {
+//  uint64_t gpr[32];
+//  uint64_t pc;
+//};
 
 void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  if (direction == DIFFTEST_TO_REF) {
-      paddr_write(addr, n ,*(uint8_t*)buf);
-  } 
-  else{
+  //if (direction == DIFFTEST_TO_REF) {
+  //    paddr_write(addr, n ,*(uint8_t*)buf);
+  //} 
+  //else{
     assert(0);
-  }
+  //}
 }
 
 void difftest_regcpy(void *dut, bool direction) {
-  struct diff_context_t* ctx = (struct diff_context_t*)dut;
-  if (direction == DIFFTEST_TO_REF) {
-    for (int i = 1; i < 32; i++) {      //i==0!!!!
-      if(i==0)
-        cpu.gpr[i] = 0;
-      else
-        cpu.gpr[i] = ctx->gpr[i];
-      printf("cpi.gpr[%d]=%ld",i,cpu.gpr[i]);
-    }
-    cpu.pc = ctx->pc;
-  } 
-  else if (direction == DIFFTEST_TO_DUT){
-    for (int i = 0; i < 32; i++) {
-      ctx->gpr[i] = cpu.gpr[i];
-    }
-    ctx->pc = cpu.pc;
-  }
+  //struct diff_context_t* ctx = (struct diff_context_t*)dut;
+  //if (direction == DIFFTEST_TO_REF) {
+  //  for (int i = 1; i < 32; i++) {      //i==0!!!!
+  //    if(i==0)
+  //      cpu.gpr[i] = 0;
+  //    else
+  //      cpu.gpr[i] = ctx->gpr[i];
+  //    printf("cpi.gpr[%d]=%ld",i,cpu.gpr[i]);
+  //  }
+  //  cpu.pc = ctx->pc;
+  //} 
+  //else if (direction == DIFFTEST_TO_DUT){
+  //  for (int i = 0; i < 32; i++) {
+  //    ctx->gpr[i] = cpu.gpr[i];
+  //  }
+  //  ctx->pc = cpu.pc;
+  //}
+  assert(0);
 }
 
 void difftest_exec(uint64_t n) {
-  cpu_exec(n);
-  //assert(0);
+  //cpu_exec(n);
+  assert(0);
 }
 
 void difftest_raise_intr(word_t NO) {
-  printf("NO=%ld",NO);
+  //printf("NO=%ld",NO);
+  assert(0);
 }
 
 void difftest_init() {
