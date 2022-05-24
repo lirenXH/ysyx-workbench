f86e71fadc6c0091e4820f37e755ece870dc67e6
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-24 14:51:46 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:51:46 up 18:40,  1 user,  load average: 0.64, 0.45, 0.26

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,13 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint64_t a = inl(0xa0000060);
+  if(a != 0){
+    kbd->keydown = 1;
+    kbd->keycode = a;
+  }
+  else{
+    kbd->keydown = 0;
+    kbd->keycode = AM_KEY_NONE;
+  }
 }
