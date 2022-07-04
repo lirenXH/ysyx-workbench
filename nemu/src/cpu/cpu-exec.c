4857927a4d9696cddcca2183b2473023a0d730d2
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-04 15:26:36 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:26:36 up 40 min,  1 user,  load average: 1.01, 0.53, 0.42

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -112,7 +112,7 @@ void cpu_exec(uint64_t n) {
   uint64_t timer_end = get_time();
   printf("timer_start2:%ld\n",timer_start);
   g_timer += timer_end - timer_start;
-
+  printf("g_timer:%ld\n",g_timer);
   switch (nemu_state.state) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
