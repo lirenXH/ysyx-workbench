7a49eb1ed2af7ff1f07737552939cbda572c8850
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-08 09:47:33 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:47:33 up 32 min,  1 user,  load average: 1.15, 0.55, 0.34

--- nemu/src/isa/riscv64/init.c
+++ nemu/src/isa/riscv64/init.c
@@ -22,7 +22,7 @@ static void restart() {
 void init_isa() {
   /* Load built-in image. */
   memcpy(guest_to_host(RESET_VECTOR), img, sizeof(img));
-
+  printf("img_size=%ld\n",sizeof(img));
   /* Initialize this virtual computer system. */
   restart();
 }
