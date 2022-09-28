0d53651dc65ee22caa376e0133da0fcc97c6623a
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-28 10:14:23 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:14:23 up 2 days, 29 min,  1 user,  load average: 0.67, 0.75, 0.68

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -20,18 +20,18 @@ const char *csrs[] = {
 void isa_reg_display() {
   int i;
   for(i=0;i<32;i++)
-    printf("%s -> 0x%08lX\n",regs[i],cpu.gpr[i]);
+    printf("%s -> 0x%16lx\n",regs[i],cpu.gpr[i]);
   for(i=0;i<=75;i++){
     if(i >= 0 && i < 7)
-      printf("%8s -> 0x%08lX  ",csrs[i],cpu.csr[i]);
+      printf("%8s -> 0x%16lx  ",csrs[i],cpu.csr[i]);
     if(i==7)
       printf("\n");
     if(i >= 64 && i < 69)
-      printf("%8s -> 0x%08lX  ",csrs[i],cpu.csr[i]);
+      printf("%8s -> 0x%16lx  ",csrs[i],cpu.csr[i]);
     if(i==69)
       printf("\n");
     if(i >= 74 && i <= 75)
-      printf("%8s -> 0x%08lX  ",csrs[i],cpu.csr[i]);
+      printf("%8s -> 0x%16lx  ",csrs[i],cpu.csr[i]);
     if(i==75)
       printf("\n");
   }
