6b7142b9f9872126c8c13807a2d8e778ac8b25cc
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-28 14:12:50 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:12:50 up 2 days,  4:27,  1 user,  load average: 0.28, 0.17, 0.12

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -6,9 +6,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
    */
   cpu.csr[65] = cpu.pc; //mepc
   cpu.csr[66] = NO;     //mcause
-  cpu.pc = epc;         //mtevc
-  printf("isa_raise_intr run epc= 0x%08lx cpu.pc = 0x%08lx \n",epc,cpu.pc);
-  return 0;
+  return epc;
 }
 
 word_t isa_query_intr() {
