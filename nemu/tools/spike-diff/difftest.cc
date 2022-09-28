385cd8e2c02b0b3777bb7a83c0842b386a59e7e0
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-28 10:53:03 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:53:03 up 2 days,  1:07,  1 user,  load average: 0.59, 0.55, 0.54

--- nemu/tools/spike-diff/difftest.cc
+++ nemu/tools/spike-diff/difftest.cc
@@ -50,7 +50,7 @@ void sim_t::diff_get_regs(void* diff_context) {
   ctx->csr[0]  = state->mstatus;
   ctx->csr[65] = state->mepc;
   ctx->csr[66] = state->mcause;
-  printf("get mstatus =  0x%016lx",state->mstatus);
+  printf("get mstatus =  0x%016lx\n",state->mstatus);
   ctx->pc = state->pc;
 }
 
@@ -59,7 +59,7 @@ void sim_t::diff_set_regs(void* diff_context) {
   for (int i = 0; i < NXPR; i++) {
     state->XPR.write(i, (sword_t)ctx->gpr[i]);
   }
-  printf("set mstatus = 0x%016lx",ctx->csr[0]);
+  printf("set mstatus = 0x%016lx\n",ctx->csr[0]);
   state->mstatus = ctx->csr[0];
   state->mepc    = ctx->csr[65];
   state->mcause  = ctx->csr[66];
