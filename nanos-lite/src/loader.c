76059178bbe2d54111b23ab55504ac4354ad7d35
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-29 16:38:46 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:38:46 up 3 days,  6:53,  1 user,  load average: 0.45, 0.59, 0.53

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,17 +10,25 @@
 #endif
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
+  int i = 0;
   Elf_Ehdr elf_E = {};
-  //Elf_Phdr elf_P = {};
+  Elf_Phdr elf_P = {};
   ramdisk_read(&elf_E , 0 , sizeof(elf_E));
-  printf("moshu is %x",32);
   assert(*(uint32_t *)elf_E.e_ident == 0x464c457f);
-  TODO();
-  return 0;
+  for(i=0;i<elf_E.e_phnum;i++){
+    uint64_t phoff_temp = elf_E.e_phoff + elf_E.e_phentsize * i;
+    ramdisk_read(&elf_P,phoff_temp,elf_E.e_phentsize);
+    if(elf_P.p_type == PT_LOAD){
+      ramdisk_read((void*)elf_P.p_paddr,elf_P.p_offset,elf_P.p_memsz);//若是load则 load对应size
+      memset((void *)(elf_P.p_vaddr + elf_P.p_filesz), 0 ,(elf_P.p_memsz - elf_P.p_filesz));  //清零 对应位置
+    }
+  }
+  //TODO();
+  return elf_E.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
-  uintptr_t entry = loader(pcb, filename);
+  uintptr_t entry = loader(pcb, filename);    //需要返回一个入口地址
   Log("Jump to entry = %p", entry);
   ((void(*)())entry) ();
 }
