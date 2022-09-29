a621251ac4a3e72d6a3ada7d730dc603256e04ce
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-29 11:16:53 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:16:53 up 3 days,  1:31,  1 user,  load average: 1.10, 0.84, 0.52

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,7 +1,6 @@
 #include <common.h>
 
 static Context* do_event(Event e, Context* c) {
-  printf("e.event = %d\n",e.event);
   switch (e.event) {
     case 1 : panic("Event ID = 1 is EVENT_YIELD!");
     default: panic("Unhandled event ID = %d", e.event);
