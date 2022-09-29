8ca977ac86847263576ba65d26069ffa8e063ce5
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-29 14:59:03 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:59:03 up 3 days,  5:13,  1 user,  load average: 1.54, 0.56, 0.28

--- nanos-lite/build/riscv64-nemu/src/loader.d
+++ nanos-lite/build/riscv64-nemu/src/loader.d
@@ -3,9 +3,16 @@
  /home/xuhao/learngit/ysyx-workbench/nanos-lite/include/proc.h \
  /home/xuhao/learngit/ysyx-workbench/nanos-lite/include/common.h \
  /home/xuhao/learngit/ysyx-workbench/abstract-machine/am/include/am.h \
+ /home/xuhao/learngit/ysyx-workbench/navy-apps/libs/libc/include/stdint.h \
+ /home/xuhao/learngit/ysyx-workbench/navy-apps/libs/libc/include/machine/_default_types.h \
+ /home/xuhao/learngit/ysyx-workbench/navy-apps/libs/libc/include/sys/features.h \
+ /home/xuhao/learngit/ysyx-workbench/navy-apps/libs/libc/include/_newlib_version.h \
+ /home/xuhao/learngit/ysyx-workbench/navy-apps/libs/libc/include/sys/_intsup.h \
+ /home/xuhao/learngit/ysyx-workbench/navy-apps/libs/libc/include/sys/_stdint.h \
  /home/xuhao/learngit/ysyx-workbench/abstract-machine/am/include/arch/riscv64-nemu.h \
  /home/xuhao/learngit/ysyx-workbench/abstract-machine/am/include/amdev.h \
  /home/xuhao/learngit/ysyx-workbench/abstract-machine/klib/include/klib.h \
  /home/xuhao/learngit/ysyx-workbench/abstract-machine/klib/include/klib-macros.h \
  /home/xuhao/learngit/ysyx-workbench/nanos-lite/include/debug.h \
- /home/xuhao/learngit/ysyx-workbench/nanos-lite/include/memory.h
+ /home/xuhao/learngit/ysyx-workbench/nanos-lite/include/memory.h \
+ /home/xuhao/learngit/ysyx-workbench/navy-apps/libs/libc/include/elf.h
