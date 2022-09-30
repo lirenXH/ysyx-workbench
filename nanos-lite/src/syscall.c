39306d3de70f76f79184303b9eef08258461a355
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-30 11:55:38 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:55:38 up 4 days,  2:10,  1 user,  load average: 0.99, 0.77, 0.61

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,16 +14,30 @@ void strace_main(uintptr_t a7,uintptr_t re){
     printf("should not come here");
     break;
   }
-  
 }
+
+size_t system_write(int fd,const void* buf,size_t len){
+  int i;
+  Log("system_write fd:%d, len:%d\n",fd,len);
+  if((fd == 1)&&(fd == 2)){
+    for(i=0;i<len;i++)
+      putch( ((char*)buf)[i] );//输出i个字符
+    return len;//返回写的字节数
+  }
+  return -1;
+}
+
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
   switch (a[0]) {
     case 0 : strace_main(a[0],c->GPRx);halt(c->GPRx);break;
     case 1 : yield();c->GPRx = 0 ;strace_main(a[0],c->GPRx);break;
-    case 4 : /*system_write();*/break;
+    case 4 : printf("do syscall_write\n");system_write((int)a[1],(void*)a[2],a[3]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
