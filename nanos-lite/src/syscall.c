9a50cf4d531b521017683763a4ca515d1902dd57
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-30 17:43:45 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:43:45 up  1:02,  1 user,  load average: 1.13, 0.72, 0.51

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -21,11 +21,10 @@ void strace_main(uintptr_t a7,uintptr_t re){
 
 size_t system_write(int fd,const void* buf,size_t len){
   int i;
-  printf("system_write fd:%d, len:%p\n",fd,len);
-  if((fd == 1)&&(fd == 2)){
+  printf("system_write fd:%p, len:%p\n",fd,len);
+  if((fd == 1)||(fd == 2)){
     for(i=0;i<len;i++){
       putch( ((char*)buf)[i] );//输出i个字符
-      printf("%d ",i);
     }
     return len;//返回写的字节数
   }
