ba224e23e84992c36c13861bf880438ed426afad
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-30 17:26:54 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:26:54 up 46 min,  1 user,  load average: 0.67, 0.56, 0.39

--- abstract-machine/am/include/arch/riscv64-nemu.h
+++ abstract-machine/am/include/arch/riscv64-nemu.h
@@ -8,8 +8,8 @@ struct Context {
 };
 
 #define GPR1 gpr[17] // a7 系统调用参数
-#define GPR2 gpr[18]
-#define GPR3 gpr[19]
-#define GPR4 gpr[20]
+#define GPR2 gpr[1]
+#define GPR3 gpr[0]
+#define GPR4 gpr[13]
 #define GPRx gpr[0]
 #endif
