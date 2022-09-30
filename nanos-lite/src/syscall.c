f80f8911da542865a94a9b3a69b065484296871f
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-30 10:44:22 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:44:22 up 4 days, 59 min,  1 user,  load average: 0.66, 0.69, 0.55

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -5,7 +5,7 @@ void strace_main(uintptr_t a7,uintptr_t re){
   switch (a7)
   {
   case 0:
-    printf("strace system call yield , ID is 0\nthe return value is %d\n\n",re);
+    printf("strace system call exit , ID is 0\nthe return value is %d\n\n",re);
     break;
   case 1:
     printf("strace system call yield , ID is 1\nthe return value is %d\n\n",re);
