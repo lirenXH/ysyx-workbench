c1c2723d864ea8c4a23bad6f8fbf1101514d209e
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-30 09:48:45 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:48:45 up 4 days, 3 min,  1 user,  load average: 0.67, 0.42, 0.17

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,10 +9,10 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->mcause) {
       case 11 :  
-        if(c->GPR1 == -1){
-          ev.event = EVENT_YIELD;//事件分发
+        if(c->GPR1 == -1){//事件二次分发
+          ev.event = EVENT_YIELD;
           c->mepc += 4;  //pc+4用于恢复上下文
-        }else
+        }else if(c->GPR1 == 1)
           ev.event = EVENT_SYSCALL;
         break;
       default: ev.event = EVENT_ERROR; break;
