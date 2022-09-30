e007766e11e52ffc6743687b6504486985044785
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-30 15:26:48 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:26:48 up 4 days,  5:41,  1 user,  load average: 0.86, 0.59, 0.42

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -61,7 +61,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  //_syscall_(SYS_write,fd,(intptr_t)buf,count);//需要带下划线 和linux区分 buf 和 intptr的类型
+  _syscall_(SYS_write,fd,(intptr_t)buf,count);//需要带下划线 和linux区分 buf 和 intptr的类型
   _exit(SYS_write);
   return 0;
 }
