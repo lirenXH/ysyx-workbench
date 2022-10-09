af59105e0900796835b005b0d3a9e08f13dcb9b7
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-09 16:32:41 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:32:41 up  5:35,  1 user,  load average: 1.74, 0.76, 0.51

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -20,8 +20,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) { //调用fs_open 传fil
   //ramdisk_read(&elf_E , 0 , sizeof(elf_E));
   assert(*(uint32_t *)elf_E.e_ident == 0x464c457f);
   for(i=0;i<elf_E.e_phnum;i++){
-    //uint64_t phoff_temp = elf_E.e_phoff + elf_E.e_phentsize * i;
-    uint64_t phoff_temp = elf_E.e_phentsize * i;
+    uint64_t phoff_temp = elf_E.e_phoff + elf_E.e_phentsize * i;
+    //uint64_t phoff_temp = elf_E.e_phentsize * i;
     fs_lseek(fd,phoff_temp,SEEK_CUR);
     //ramdisk_read(&elf_P,phoff_temp,elf_E.e_phentsize);
     fs_read(fd, &elf_P, elf_E.e_phentsize);
