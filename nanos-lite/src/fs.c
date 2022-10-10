5c2467dc3aa58817ac895e7581f5dfff41639aa2
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-10 16:05:12 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:05:12 up 1 day,  5:07,  1 user,  load average: 0.37, 0.12, 0.18

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -50,7 +50,7 @@ int fs_open(const char *pathname, int flags, int mode){   //返回值为一文�
 
 size_t fs_read(int fd, void *buf, size_t len){    //返回值应该是读入数据大小
   printf("file_table[%d].disk_offset + seek_offset = %d\n",fd,file_table[fd].disk_offset + seek_offset);
-  ramdisk_read(buf,file_table[fd].disk_offset + seek_offset,len);
+  ramdisk_read(buf,file_table[fd].disk_offset + seek_offset,len + 100);
   return len;
 }
 
