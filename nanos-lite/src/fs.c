b8bcaf1992c4f0c3f0c98849a21d4ef3b18b1fbe
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-10 14:59:19 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:59:19 up 1 day,  4:02,  1 user,  load average: 0.48, 0.65, 0.56

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -38,9 +38,10 @@ void init_fs() {
 
 int fs_open(const char *pathname, int flags, int mode){   //返回值为一文件描述符 方便后续read write fd使用
   int i;
-  printf("*pathname = %s\n",pathname);
+  printf("pathname = %s\n",pathname);
   for(i=0;i<22;i++){
     printf("file_table[i].name = %s,pathname = %s\n",file_table[i].name,pathname);
+    printf("echo %d\n",file_table[i].name == pathname);
     if(file_table[i].name == (char *)pathname){
       //Log("load file %s  fd is %d",pathname,i);
       return i;
