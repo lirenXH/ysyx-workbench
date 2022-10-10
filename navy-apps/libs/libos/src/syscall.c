c9a621950f94d90e2d2deaa85405b6aba0c96bbd
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-10 11:53:16 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:53:16 up 1 day, 55 min,  1 user,  load average: 0.74, 0.57, 0.43

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -58,7 +58,7 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) { //打开一个文件, 并返回相应的文件描述符
-  return _syscall_(SYS_open,(intptr_t)path,(intptr_t)flags,mode);
+  return _syscall_(SYS_open,*path,(intptr_t)flags,mode);
   //_exit(SYS_open);
 }
 
