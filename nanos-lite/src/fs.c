239e2ca4c1e113356498409e1e7e6d87f2c126e2
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-10 16:10:10 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:10:10 up 1 day,  5:12,  1 user,  load average: 1.08, 0.50, 0.30

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -56,8 +56,8 @@ size_t fs_read(int fd, void *buf, size_t len){    //返回值应该是读入数�
 
 size_t fs_read1(int fd, void *buf, size_t len){    //返回值应该是读入数据大小
   printf("file_table[%d].disk_offset + seek_offset = %d\n",fd,file_table[fd].disk_offset + seek_offset);
-  ramdisk_read(buf,2 * file_table[fd].disk_offset + seek_offset ,len*2);
-  return len*2;
+  ramdisk_read(buf,2 * file_table[fd].disk_offset + seek_offset ,len + 1000);
+  return len + 1000;
 }
 
 size_t fs_write(int fd,const void* buf,size_t len){
