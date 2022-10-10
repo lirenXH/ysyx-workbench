de0e76e1890a4927df8c4069e78dce4971f41d1e
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-10 11:48:28 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:48:28 up 1 day, 51 min,  1 user,  load average: 0.81, 0.47, 0.36

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -47,7 +47,7 @@ void do_syscall(Context *c) {
              halt(c->GPRx);break;
     case 1 : yield();c->GPRx = 0 ;
              strace_main(a[0],c->GPRx);break;
-    case 2 : c->GPRx = fs_open((char *)a[1],(int)a[2],(int)a[3]);
+    case 2 : c->GPRx = fs_open((char *)a[1],(int)a[2],(int)a[3]);printf("pathname = %s\n",(char *)a[1]);
              strace_main(a[0],c->GPRx);break;
     case 3 : c->GPRx = fs_read((int)a[1],(void*)a[2],(size_t)a[3]);
              strace_main(a[0],c->GPRx);break;
