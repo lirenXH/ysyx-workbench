b4414c6a0d911736c02cc1a3b786aab7d8b9cc52
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-10 14:07:46 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:07:46 up 1 day,  3:10,  1 user,  load average: 0.56, 0.39, 0.36

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -41,7 +41,6 @@
 #endif
 
 extern char _end;
-intptr_t end = (intptr_t)&_end;         //program break一开始的位置位于_end
 
 intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
   register intptr_t _gpr1 asm (GPR1) = type;
@@ -69,6 +68,7 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {         //返回-1表示堆区调整失败
+  intptr_t end = (intptr_t)&_end;         //program break一开始的位置位于_end
   intptr_t old = end;                     //记录旧program break的位置
   intptr_t new = end + increment;         //被调用时, 根据记录的program break位置和参数increment, 计算出新program break
   if(_syscall_(SYS_brk,new,0,0) == 0){    //通过SYS_brk系统调用来让操作系统设置新program break , 若SYS_brk系统调用成功, 该系统调用会返回0
