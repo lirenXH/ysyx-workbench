f71700db0e04af662a54a4d94c4e998330694c4c
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-11 14:54:47 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:54:47 up 2 days,  3:57,  1 user,  load average: 0.74, 0.60, 0.28

--- navy-apps/tests/timer-test/timer.c
+++ navy-apps/tests/timer-test/timer.c
@@ -4,8 +4,15 @@
 int main() {
 
     struct timeval tv_begin;
+    struct timeval tv_end;
     gettimeofday(&tv_begin,NULL);
-    printf("tv__sec_:%ld\n",tv_begin.tv_sec);
-    printf("tv__usec:%ld\n",tv_begin.tv_usec);
+    gettimeofday(&tv_end,NULL);
+    for(int i = 0 ;i < 10 ; i++){
+        int cha = tv_end.tv_sec - tv_begin.tv_sec;
+        while(cha != 1)
+            gettimeofday(&tv_end,NULL);
+        gettimeofday(&tv_begin,NULL);
+        printf("gettimeofday 1s\n");
+    }
     return 0;
 }
