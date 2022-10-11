7ee6039aee69b7fc70d9ebbd2ab7651e1e80f22b
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-11 16:59:58 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:59:58 up 2 days,  6:02,  1 user,  load average: 0.94, 1.08, 1.00

--- navy-apps/tests/timer-test/timer.c
+++ navy-apps/tests/timer-test/timer.c
@@ -7,16 +7,15 @@ int main() {
     struct timeval tv_end;
     gettimeofday(&tv_begin,NULL);
     gettimeofday(&tv_end,NULL);
-    printf("tv_begin_sec:%d\n",tv_begin.tv_sec);
-    printf("tv_end___sec:%d\n",tv_end.tv_sec);
+    //printf("tv_begin_sec:%d\n",tv_begin.tv_sec);
+    //printf("tv_end___sec:%d\n",tv_end.tv_sec);
     for(int i = 0 ;i < 10 ; i++){
-        int cha = tv_end.tv_sec - tv_begin.tv_sec;
-        printf("tv_begin_usec:%d\n",tv_begin.tv_usec);
-        printf("tv_end___usec:%d\n",tv_end.tv_usec);
-        while(cha != 1){
+        while(1){
+            int cha = 0;
             gettimeofday(&tv_end,NULL);
-            printf("tv_end___sec:%d\n",tv_end.tv_sec);
-            printf("tv_end__usec:%d\n",tv_end.tv_usec);
+            cha = tv_end.tv_sec - tv_begin.tv_sec;
+            if(cha == 1)
+            break;
         }
         gettimeofday(&tv_begin,NULL);
         printf("gettimeofday 1s\n");
