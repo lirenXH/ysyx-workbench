cf6a5d89ea3290e0200588c129fdfdacad5cb23c
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-11 16:44:11 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:44:11 up 2 days,  5:46,  1 user,  load average: 1.47, 0.99, 0.86

--- navy-apps/tests/timer-test/timer.c
+++ navy-apps/tests/timer-test/timer.c
@@ -11,6 +11,8 @@ int main() {
     printf("tv_end___sec:%d\n",tv_end.tv_sec);
     for(int i = 0 ;i < 10 ; i++){
         int cha = tv_end.tv_sec - tv_begin.tv_sec;
+        printf("tv_begin_sec:%d\n",tv_begin.tv_sec);
+        printf("tv_end___sec:%d\n",tv_end.tv_sec);
         while(cha != 1){
             gettimeofday(&tv_end,NULL);
             //printf("cha = %d\n",cha);
