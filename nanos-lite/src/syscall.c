9c06cac208190f2d625e2027416b23c91c1f16dc
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-11 16:23:45 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:23:45 up 2 days,  5:26,  1 user,  load average: 0.47, 0.78, 0.74

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -65,7 +65,7 @@ void do_syscall(Context *c) {
              strace_main(a[0],c->GPRx);break;
     case 9 : c->GPRx = 0;
              strace_main(a[0],c->GPRx);break;    //只需要让SYS_brk系统调用总是返回0即可
-    case 19 : c->GPRx = 0; get_ttime(a[1],a[2]);c->GPR3 = io_read(AM_TIMER_UPTIME).us;
+    case 19 : c->GPRx = 0; get_ttime(a[1],a[2]);
              break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
