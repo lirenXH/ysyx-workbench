58b380b79ffd1923ef71b67e776fc229b9046ed4
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-12 10:36:15 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:36:15 up  1:03,  1 user,  load average: 1.15, 0.79, 0.59

--- navy-apps/tests/timer-test/timer.c
+++ navy-apps/tests/timer-test/timer.c
@@ -1,18 +1,22 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/time.h>
+#include <NDL.h>
 int main() {
 
     struct timeval tv_begin;
     struct timeval tv_end;
     gettimeofday(&tv_begin,NULL);
-    gettimeofday(&tv_end,NULL);
+    //gettimeofday(&tv_end,NULL);
     //printf("tv_begin_sec:%d\n",tv_begin.tv_sec);
     //printf("tv_end___sec:%d\n",tv_end.tv_sec);
+    NDL_Init(0);
+
     for(int i = 0 ;i < 10 ; i++){
         while(1){
             int cha = 0;
-            gettimeofday(&tv_end,NULL);
+            //gettimeofday(&tv_end,NULL);
+            tv_end.tv_sec = NDL_GetTicks() / 1000000;
             cha = tv_end.tv_sec - tv_begin.tv_sec;
             if(cha == 1)
             break;
@@ -21,6 +25,6 @@ int main() {
         printf("gettimeofday %ds\n",i);
     }
 
-    //NDL_Init(0);
+    
     return 0;
 }
