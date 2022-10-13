2dffdb3c4ba7747ee9c5dba36b83719fce180a82
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-13 16:05:45 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:05:45 up 1 day,  6:32,  1 user,  load average: 1.50, 0.47, 0.40

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -61,7 +61,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     fseek(vga_fd, w*i*32, SEEK_SET);       //写完一行后设置画布指针在下一行的初始位置
     for(int j = x ; j < (x+w) ; j++){      //从x开始写w个
       //printf("write i = %d , j = %d pix = %d\n",i,j,pixels[i]);
-      write(4,pixels,32);      //写入像素
+      fwrite(vga_fd,pixels,32);      //写入像素
     }
   }
 }
