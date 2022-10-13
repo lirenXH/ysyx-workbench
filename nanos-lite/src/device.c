9d091a66ab4eff943126015a2ac4b2cffd41352b
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-13 16:18:59 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:18:59 up 1 day,  6:45,  1 user,  load average: 1.30, 1.07, 0.82

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -25,6 +25,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 size_t events_read(void *buf, size_t offset, size_t len) {
 
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+  printf("ev.keycode = %d\n",ev.keycode );
   if(ev.keycode == 0){
     return 0;
   }else if(ev.keydown){
--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -81,7 +81,7 @@ int NDL_QueryAudio() {
 }
 
 int NDL_Init(uint32_t flags) {
-  keyboard_fd = fopen("/dev/events","r+");
+  keyboard_fd = open("/dev/events","r+");
   if (getenv("NWM_APP")) {
     evtdev = 3;
   }
