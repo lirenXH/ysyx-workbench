7277eb6d2696421a7c21603b7c40e9c7fbe85adc
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-14 10:45:41 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:45:41 up 2 days,  1:12,  1 user,  load average: 1.41, 0.96, 0.82

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -53,6 +53,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   printf("x = %d,y = %d,w = %d,h = %d\n",ctl.x,ctl.y,ctl.w,ctl.h);
   printf("pixels = %x,sync = %d\n",ctl.pixels,ctl.sync);
   ctl.sync = true;
+  printf("sync2 = %d\n",ctl.sync);
   return 0;
 }
 
