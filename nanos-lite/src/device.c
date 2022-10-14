ad7d12ea055e363c1702546b9b84b7fc74e2700e
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-14 10:44:40 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:44:40 up 2 days,  1:11,  1 user,  load average: 0.61, 0.72, 0.74

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -48,9 +48,11 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   printf("offset = %d , len = %d\n",offset,len);
   //strncpy(fb_canva,buf,32);
   //fb_canva[1] = 0;
+  //uint32_t *f = (uint32_t*)(uintptr_t)FB_ADDR;
   AM_GPU_FBDRAW_T ctl = io_read(AM_GPU_FBDRAW);
   printf("x = %d,y = %d,w = %d,h = %d\n",ctl.x,ctl.y,ctl.w,ctl.h);
   printf("pixels = %x\n",ctl.pixels);
+  ctl.sync = true;
   return 0;
 }
 
