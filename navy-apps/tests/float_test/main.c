f03ddeea392cd5874aec06c92d7cdbb116949086
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-17 14:35:15 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:35:15 up  4:03,  1 user,  load average: 0.86, 0.56, 0.40

--- navy-apps/tests/float_test/main.c
+++ navy-apps/tests/float_test/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <fixedptc.h>
 
 int main() {
@@ -11,7 +12,9 @@ int main() {
       c = fixedpt_toint(fixedpt_div(fixedpt_mul(a + FIXEDPT_ONE, b), fixedpt_rconst(2.3)));
     }
     printf("c = %d\n",c);
-    fixedpt aa = fixedpt_rconst(1);
-    printf("aa floor = %d\n",fixedpt_floor(aa));
+    int aa = 1.2;
+    fixedpt AA = fixedpt_rconst(aa);
+    printf("AA dut floor = %d\n",fixedpt_floor(aa));
+    printf("AA ref floor = %d\n",floor(aa));
     return 0;
 }
