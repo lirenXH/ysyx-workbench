36f1f0650897dc1c579ac49f1992f7c7354c0130
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-17 19:42:14 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:42:14 up  9:10,  1 user,  load average: 1.32, 0.88, 0.85

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -12,6 +12,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
   printf("dst->w = %d,dst->h = %d\n",dst->w,dst->h);
   uint32_t *pixels_fill = malloc(dst->w * dst->h * sizeof(uint32_t));
+  printf("dst->w = %d,dst->h = %d\n",dst->w,dst->h);
   for(int i=0;i<dstrect->h;i++){
     for(int j=0;j<dstrect->w;j++){
       pixels_fill[dst->w * i + j] = color;
