f1644e0fee9cb3ba81441fa6bddc4798f71c49e3
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-17 15:53:08 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:53:08 up  5:21,  1 user,  load average: 0.75, 0.99, 0.77

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -13,7 +13,7 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
-  printf("SDL_Surface.w = %d,SDL_Surface.h = %d\nx = %d,y = %d,w = %d,h = %d\n",SDL_Surface.w,SDL_Surface.h,x,y,w,h);
+  printf("SDL_Surface.w = %d,SDL_Surface.h = %d\nx = %d,y = %d,w = %d,h = %d\n",s->w,s->h,x,y,w,h);
   //printf("SDL_UpdateRect NO\n");
 }
 
