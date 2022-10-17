0ca493b7a061a055bf3aecfbafb732edf07eae9f
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-17 14:10:52 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:10:52 up  3:39,  1 user,  load average: 0.39, 0.38, 0.36

--- navy-apps/tests/float_test/main.c
+++ navy-apps/tests/float_test/main.c
@@ -2,11 +2,12 @@
 #include <fixedptc.h>
 
 int main() {
-  fixedpt a = fixedpt_rconst(1.2);
-  printf("a = %d\n",a);
-  int b = 0;
-  //fixedpt_fromint(a);
-  b = fixedpt_floor(fixedpt_fromint(a));
-  printf("b = %d\n",b);
-  return 0;
+    fixedpt a = fixedpt_rconst(1.2);
+    fixedpt b = fixedpt_fromint(10);
+    int c = 0;
+    if (b > fixedpt_rconst(7.9)) {
+      c = fixedpt_toint(fixedpt_div(fixedpt_mul(a + FIXEDPT_ONE, b), fixedpt_rconst(2.3)));
+    }
+     printf("c = %d\n",c);
+     return 0;
 }
