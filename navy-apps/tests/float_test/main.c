779fef3dbf8f0fa0a4253ec7d47d7a6da39509d0
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-17 14:56:37 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:56:37 up  4:25,  1 user,  load average: 1.00, 0.86, 0.70

--- navy-apps/tests/float_test/main.c
+++ navy-apps/tests/float_test/main.c
@@ -26,6 +26,6 @@ int main() {
     if (b > fixedpt_rconst(7.9)) {
       c = fixedpt_toint(fixedpt_divi(fixedpt_muli(a + FIXEDPT_ONE, b), fixedpt_rconst(2.3)));
     }
-
+    printf("c = %d\n",c);
     return 0;
 }
