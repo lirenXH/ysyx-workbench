3f18159499ceaef4ec3937aa9119471f7825a594
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-17 14:04:31 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:04:31 up  3:33,  1 user,  load average: 0.98, 0.68, 0.42

--- navy-apps/tests/float_test/main.c
+++ navy-apps/tests/float_test/main.c
@@ -2,7 +2,7 @@
 #include <fixedptc.h>
 
 int main() {
-  float a =23.51234141341;
+  float a =3.14159265358979323846;
   int b = 0;
   //fixedpt_fromint(a);
   b = fixedpt_floor(fixedpt_fromint(a));
