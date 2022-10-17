2c64cc13820256f8135418a6f5fa915f83f5cb3b
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-17 18:01:46 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:01:46 up  7:30,  1 user,  load average: 0.65, 0.85, 0.73

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -21,8 +21,8 @@ int SDL_PollEvent(SDL_Event *ev) {
 int SDL_WaitEvent(SDL_Event *event) {
   char buf[64];
   if(NDL_PollEvent(buf, sizeof(buf))){
-    for(int i=0;i<5;i++)
-      printf("%d\n",buf[i]);
+    for(int i=3;i<5;i++)
+      printf("%d\n",buf[i]-'0'); 
     //if(buf[1]==)
     //  event->type = SDL_KEYDOWN;
     //event->key.keysym.sym = SDLK_DOWN;
