48f944415c0ead0c87c747c2ce4fd2e8d685de67
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-17 15:36:43 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:36:43 up  5:05,  1 user,  load average: 1.44, 0.59, 0.41

--- navy-apps/tests/float_test/main.c
+++ navy-apps/tests/float_test/main.c
@@ -2,15 +2,15 @@
 #include <fixedptc.h>
 
 int main() {
-    fixedpt a = fixedpt_rconst(1.2);
-    printf("a = %d\n",a);
-    fixedpt b = fixedpt_fromint(10);
-    printf("b = %d\n",b);
-    int c = 0;
-    if (b > fixedpt_rconst(7.9)) {
-      c = fixedpt_toint(fixedpt_div(fixedpt_mul(a + FIXEDPT_ONE, b), fixedpt_rconst(2.3)));
-    }
-    printf("c = %d\n",c);
+    //fixedpt a = fixedpt_rconst(1.2);
+    //printf("a = %d\n",a);
+    //fixedpt b = fixedpt_fromint(10);
+    //printf("b = %d\n",b);
+    //int c = 0;
+    //if (b > fixedpt_rconst(7.9)) {
+    //  c = fixedpt_toint(fixedpt_div(fixedpt_mul(a + FIXEDPT_ONE, b), fixedpt_rconst(2.3)));
+    //}
+    //printf("c = %d\n",c);
     float aa = 3;
     fixedpt AA = fixedpt_rconst(aa);
     printf("AA dut floor = %d\n",fixedpt_floor(AA));
