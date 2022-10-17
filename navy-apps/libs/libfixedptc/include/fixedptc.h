b4315f43ab0f25a3d53225c60511dfca1c776c5b
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-17 14:46:23 +0800

    >  compile NEMU ysyx_22040759 许浩 Linux ubuntu 5.11.0-49-generic #55-Ubuntu SMP Wed Jan 12 17:36:34 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:46:23 up  4:14,  1 user,  load average: 0.84, 0.73, 0.55

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -158,7 +158,7 @@ static inline fixedpt fixedpt_floor(fixedpt A) {
 
 static inline fixedpt fixedpt_ceil(fixedpt A) {
 	fixedpt C = fixedpt_sub(A,fixedpt_floor(A));
-	printf("C = %d\n",C);
+	printf("A = %d A_floor = %d C = %d\n",A,fixedpt_floor(A),C);
 	if(C == 0) 
 		return fixedpt_toint(A);
 	else
